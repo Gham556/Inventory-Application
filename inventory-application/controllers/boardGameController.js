b5f002658ac129001f4f9c186fedeba22c72159e
George <georgea.fotheringham@gmail.com>
2022-10-10 17:22:59 -0400

    adds boardGame create get/post, needs view

--- inventory-application/controllers/boardGameController.js
+++ inventory-application/controllers/boardGameController.js
@@ -61,13 +61,95 @@ exports.boardGame_detail = (req, res, next) => {
 
 // Display boardGame create form on GET.
 exports.boardGame_create_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: boardGame create GET");
+    async.parallel({
+        devloper(callback) {
+            Developer.find(callback);
+        },
+        genre(callback){
+            Genre.find(callback)
+        },
+    }, (err, results) => {
+        if(err) {
+            return next(err);
+        }
+    res.render("boardGame_form", 
+    { title: "Create Board Game",
+      developer: results.devloper,
+      genre: results.genre,  
+     });
+
+    })
+ 
 };
 
 // Handle boardGame create on POST.
-exports.boardGame_create_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: boardGame create POST");
-};
+exports.boardGame_create_post = [
+    //convert genre to array
+    (req, res, next) => {
+        if(!Array.isArray(req.body.genre)) {
+            req.body.genre = 
+                typeof req.body.genre === "undefined" ? [] : [req.body.genre]
+        }
+        next();
+    },
+
+    body("title").trim().isLength({ min: 1 }).escape().withMessage("Title Must Be Specified"),
+    body("playerCount").trim().isLength({ min: 1 }).escape().withMessage("Player Count Must Be Specified"),
+    body("playTime").trim().isLength({ min: 1 }).escape().withMessage("Play Time Must Be Specified"),
+    body("developer").trim().isLength({ min: 1 }).escape().withMessage("Developer Must Be Specified"),
+    body("genre.*").escape(),
+    body("price").trim().isLength({ min: 1}).escape(),
+
+    //proccess req after validation and sanitation
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        const boardGame = new BoardGame({
+            title: req.body.title,
+            playerCount: req.body.playerCount,
+            playTime: req.body.playTime,
+            developer: req.body.developer,
+            genre: req.body.genre,
+            price: req.body.price,
+        });
+
+        if(!errors.isEmpty()) {
+            async.parallel({
+                devloper(callback) {
+                    Developer.find(callback);
+                },
+                genre(callback){
+                    Genre.find(callback)
+                },
+            }, (err, results) => {
+                if(err) {
+                    return next(err);
+                }
+
+            for(const genre of results.genres) {
+                if(boardGame.genre.includes(genre._id)){
+                    genre.checked = "true";
+                }
+            }
+            res.render("boardGame_form", 
+            { title: "Create Board Game",
+              developer: results.devloper,
+              genre: results.genre,
+              boardGame, 
+              errors: errors.array(),  
+             });
+            })
+            return;
+        }
+
+        boardGame.save((err) => {
+            if(err) {
+                return next(err);
+            }
+            res.redirect(boardGame.url);
+        });
+    },
+]
 
 // Display boardGame delete form on GET.
 exports.boardGame_delete_get = (req, res) => {
